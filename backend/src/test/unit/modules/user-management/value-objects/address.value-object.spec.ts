49a599b9e6edcc8d637be47d4ac50f0b553ac15e
PauloCSantos <paulopcsj@hotmail.com>
2024-01-09 09:43:52 -0300

    test: implementado testes unitarios para o modulo user-management entidades e objetos de valores

---
+++ backend/src/test/unit/modules/user-management/value-objects/address.value-object.spec.ts
@@ -0,0 +1,121 @@
+import Address from '@/modules/user-management/@shared/domain/value-object/address.value-object';
+
+describe('Address unit test', () => {
+  describe('On fail', () => {
+    it('should throw an error if any address field is missing', () => {
+      const invalidAddressData = {
+        street: 'Main Street',
+        zip: '12345',
+        number: 123,
+        avenue: 'Central Avenue',
+        state: 'California',
+      };
+      //@ts-expect-error
+      expect(() => new Address(invalidAddressData)).toThrow(
+        'All address fields are mandatory'
+      );
+    });
+    it('should throw an error if any address field is empty', () => {
+      const invalidAddressData = {
+        street: 'Main Street',
+        city: 'Cityville',
+        number: 123,
+        zip: '',
+        avenue: 'Central Avenue',
+        state: 'California',
+      };
+      expect(() => new Address(invalidAddressData)).toThrow(
+        'All address fields are mandatory'
+      );
+    });
+    it('should throw an error when update an address object with invalid input', () => {
+      const addressData = {
+        street: 'Street A',
+        city: 'City A',
+        zip: '111111-111',
+        number: 1,
+        avenue: 'Avenue A',
+        state: 'State A',
+      };
+      const address = new Address(addressData);
+      const updateAddress = {
+        street: 'S',
+        city: '',
+        zip: '111111-222',
+        number: 'asd',
+        avenue:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis velit, lacinia ac leo eget, facilisis faucibus lectus. Integer in dolor ac ante tincidunt suscipit a vel purus. Suspendisse eu faucibus ipsum. Aliquam egestas pulvinar ultrices. Cras fusce.',
+        state: 'State B',
+      };
+
+      expect(() => (address.street = updateAddress.street)).toThrow(
+        'The street field was not filled in correctly'
+      );
+      expect(() => (address.city = updateAddress.city)).toThrow(
+        'The city field was not filled in correctly'
+      );
+      expect(() => (address.avenue = updateAddress.avenue)).toThrow(
+        'The avenue field was not filled in correctly'
+      );
+      //@ts-expect-error
+      expect(() => (address.number = updateAddress.number)).toThrow(
+        'The number field must be of numeric type'
+      );
+    });
+  });
+
+  describe('On success', () => {
+    it('should create an address object with valid input', () => {
+      const addressData = {
+        street: 'Street A',
+        city: 'City A',
+        zip: '111111-111',
+        number: 1,
+        avenue: 'Avenue A',
+        state: 'State A',
+      };
+
+      const address = new Address(addressData);
+      expect(address).toBeInstanceOf(Address);
+      expect(address.street).toBe(addressData.street);
+      expect(address.city).toBe(addressData.city);
+      expect(address.zip).toBe(addressData.zip);
+      expect(address.number).toBe(addressData.number);
+      expect(address.avenue).toBe(addressData.avenue);
+      expect(address.state).toBe(addressData.state);
+    });
+    it('should update an address object with valid input', () => {
+      const addressData = {
+        street: 'Street A',
+        city: 'City A',
+        zip: '111111-111',
+        number: 1,
+        avenue: 'Avenue A',
+        state: 'State A',
+      };
+      const address = new Address(addressData);
+      const updateAddress = {
+        street: 'Street B',
+        city: 'City B',
+        zip: '111111-222',
+        number: 2,
+        avenue: 'Avenue B',
+        state: 'State B',
+      };
+      address.street = updateAddress.street;
+      address.city = updateAddress.city;
+      address.zip = updateAddress.zip;
+      address.number = updateAddress.number;
+      address.avenue = updateAddress.avenue;
+      address.state = updateAddress.state;
+
+      expect(address).toBeInstanceOf(Address);
+      expect(address.street).toBe(updateAddress.street);
+      expect(address.city).toBe(updateAddress.city);
+      expect(address.zip).toBe(updateAddress.zip);
+      expect(address.number).toBe(updateAddress.number);
+      expect(address.avenue).toBe(updateAddress.avenue);
+      expect(address.state).toBe(updateAddress.state);
+    });
+  });
+});
---
+++ backend/src/test/unit/modules/user-management/value-objects/salary.value-object.spec.ts
@@ -0,0 +1,72 @@
+import Salary from '@/modules/user-management/@shared/domain/value-object/salary.value-object';
+
+describe('Salary unit test', () => {
+  describe('On fail', () => {
+    it('should throw an error for non-numeric salary', () => {
+      expect(() => {
+        new Salary({
+          //@ts-expect-error
+          salary: '1200',
+          currency: 'R$',
+        });
+      }).toThrow('The salary field must be of numeric type');
+    });
+
+    it('should throw an error for salary less than or equal to zero', () => {
+      expect(() => {
+        new Salary({
+          salary: 0,
+          currency: 'R$',
+        });
+      }).toThrow('Salary must be greater than zero');
+    });
+
+    it('should throw an error for invalid currency', () => {
+      expect(() => {
+        new Salary({
+          salary: 2000,
+          //@ts-expect-error
+          currency: 'YEN',
+        });
+      }).toThrow('This currency is not accepted');
+    });
+  });
+
+  describe('On success', () => {
+    it('should create a Salary instance with valid input', () => {
+      const salaryInstance = new Salary({
+        salary: 1000,
+        currency: 'R$',
+      });
+      expect(salaryInstance).toBeInstanceOf(Salary);
+    });
+
+    it('should set default currency if not provided', () => {
+      const validSalaryWithoutCurrency = {
+        salary: 1500,
+      };
+      const salaryInstance = new Salary(validSalaryWithoutCurrency);
+      expect(salaryInstance.currency).toBe('R$');
+    });
+
+    it('should calculate total income with currency symbol and salary', () => {
+      const salaryInstance = new Salary({
+        salary: 2500,
+        currency: '€',
+      });
+      expect(salaryInstance.calculateTotalIncome()).toBe('€:2500');
+    });
+
+    it('should increase salary by a given percentage', () => {
+      const salary = new Salary({ salary: 3000, currency: 'R$' });
+      salary.increaseSalary(10);
+      expect(salary.salary).toBe(3300);
+    });
+
+    it('should decrease salary by a given percentage', () => {
+      const salary = new Salary({ salary: 4000, currency: '€' });
+      salary.decreaseSalary(20);
+      expect(salary.salary).toBe(3200);
+    });
+  });
+});
