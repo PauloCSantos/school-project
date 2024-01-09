ff5f5719d8a9a1bf69247eb68689cd3262e9535f
PauloCSantos <paulopcsj@hotmail.com>
2024-01-09 15:24:56 -0300

    chore: retirei a validacao do id para o meu validations.ts para poder usar quando necessario

--- backend/src/modules/@shared/domain/value-object/id.value-object.ts
+++ backend/src/modules/@shared/domain/value-object/id.value-object.ts
@@ -1,3 +1,5 @@
+import { validId } from '@/util/validations';
+
 export default class Id {
   private _id: string;
 
@@ -15,11 +17,7 @@ export default class Id {
   }
 
   private validate(id: string): void {
-    if (
-      !id.match(
-        /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
-      )
-    ) {
+    if (!validId(id)) {
       throw new Error('ID inválido');
     }
   }
--- backend/src/util/validations.ts
+++ backend/src/util/validations.ts
@@ -58,3 +58,10 @@ export function validCNPJ(value: string): boolean {
   }
   return true;
 }
+
+export function validId(value: string): boolean {
+  const regexMatch = value.match(
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+  );
+  return !!regexMatch;
+}
