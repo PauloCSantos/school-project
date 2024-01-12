ef856134bb648ff4c4965ce4be979823e630f02d
PauloCSantos <paulopcsj@hotmail.com>
2024-01-12 15:07:37 -0300

    test: realizado os teste do modulo que gerencia avaliacoes, notas e chamada

---
+++ backend/src/test/unit/modules/evaluation-note-attendance-management/entities/evaluation.entity.spec.ts
@@ -0,0 +1,45 @@
+import Id from '@/modules/@shared/domain/value-object/id.value-object';
+import Evaluation from '@/modules/evaluation-note-attendance-management/evaluation/domain/entity/evaluation.entity';
+
+describe('Evaluation unit test', () => {
+  const evaluationData = {
+    teacher: new Id().id,
+    lesson: new Id().id,
+    type: 'Exame',
+    value: 10,
+  };
+
+  describe('On fail', () => {
+    it('should throw error when the input have missing values', () => {
+      expect(() => {
+        //@ts-expect-error
+        new Evaluation({});
+      }).toThrow('All evalutation fields are mandatory');
+    });
+    it('Error when setting invalid type', () => {
+      const evaluationInstance = new Evaluation(evaluationData);
+
+      expect(() => {
+        evaluationInstance.type = '';
+      }).toThrow('Type field is not valid');
+    });
+    it('Error when setting invalid value', () => {
+      const evaluationInstance = new Evaluation(evaluationData);
+
+      expect(() => {
+        evaluationInstance.value = 80;
+      }).toThrow('The value field must be numeric and between 0 and 10');
+    });
+  });
+  describe('On success', () => {
+    it('Create a valid evaluation', () => {
+      const evaluationInstance = new Evaluation(evaluationData);
+      expect(evaluationInstance).toBeInstanceOf(Evaluation);
+      expect(evaluationInstance.id).toBeDefined;
+      expect(evaluationInstance.teacher).toBe(evaluationData.teacher);
+      expect(evaluationInstance.type).toBe(evaluationData.type);
+      expect(evaluationInstance.lesson).toBe(evaluationData.lesson);
+      expect(evaluationInstance.value).toBe(evaluationData.value);
+    });
+  });
+});
---
+++ backend/src/test/unit/modules/evaluation-note-attendance-management/entities/note.entity.spec.ts
@@ -0,0 +1,43 @@
+import Id from '@/modules/@shared/domain/value-object/id.value-object';
+import Note from '@/modules/evaluation-note-attendance-management/note/domain/entity/note.entity';
+
+describe('Note unit test', () => {
+  const noteData = {
+    id: new Id(),
+    evaluation: new Id().id,
+    student: new Id().id,
+    note: 8.5,
+  };
+
+  describe('On fail', () => {
+    it('should throw error when the input have missing values', () => {
+      //@ts-expect-error
+      expect(() => new Note({})).toThrow('All note fields are mandatory');
+    });
+    it('should throw error when setting an invalid note', () => {
+      const note = new Note(noteData);
+
+      expect(() => {
+        note.note = 14;
+      }).toThrow('The note field must be numeric and between 0 and 10');
+    });
+  });
+
+  describe('On success', () => {
+    it('Should create a note with valid input', () => {
+      const note = new Note(noteData);
+
+      expect(note).toBeInstanceOf(Note);
+      expect(note.id).toBe(noteData.id);
+      expect(note.evaluation).toBe(noteData.evaluation);
+      expect(note.student).toBe(noteData.student);
+      expect(note.note).toBe(noteData.note);
+    });
+    it('Should update values', () => {
+      const noteInstance = new Note(noteData);
+      noteInstance.note = 9.0;
+
+      expect(noteInstance.note).toBe(9.0);
+    });
+  });
+});
