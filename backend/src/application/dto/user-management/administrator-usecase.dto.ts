f986447bf1048a11510ec03fd98ea47f426993df
PauloCSantos <paulopcsj@hotmail.com>
2024-01-19 09:20:05 -0300

    feat: Comecando a codificar os dtos para os usecases do administrator

---
+++ backend/src/application/dto/user-management/administrator-usecase.dto.ts
@@ -0,0 +1,16 @@
+export interface FindAdministratorInputDto {
+  id: string;
+}
+export interface FindAdministratorOutputDto {}
+
+export interface FindAllAdministratorInputDto {}
+export interface FindAllAdministratorOutputDto {}
+
+export interface CreateAdministratorInputDto {}
+export interface CreateAdministratorOutputDto {}
+
+export interface UpdateAdministratorInputDto {}
+export interface UpdateAdministratorOutputDto {}
+
+export interface DeleteAdministratorInputDto {}
+export interface DeleteAdministratorOutputDto {}
