1512e901e22ad082e57ca9a8aa832abb10d669f7
PauloCSantos <paulopcsj@hotmail.com>
2024-01-26 16:45:44 -0300

    feat: Implementado os usecases de schedule

---
+++ backend/src/application/usecases/schedule-lesson-management/schedule/findSchedule.usecase.ts
@@ -0,0 +1,31 @@
+import {
+  FindScheduleInputDto,
+  FindScheduleOutputDto,
+} from '@/application/dto/schedule-lesson-management/schedule-usecase.dto';
+import UseCaseInterface from '../../@shared/use-case.interface';
+import ScheduleGateway from '@/modules/schedule-lesson-management/schedule/gateway/schedule.gateway';
+
+export default class FindSchedule
+  implements
+    UseCaseInterface<FindScheduleInputDto, FindScheduleOutputDto | undefined>
+{
+  private _scheduleRepository: ScheduleGateway;
+
+  constructor(scheduleRepository: ScheduleGateway) {
+    this._scheduleRepository = scheduleRepository;
+  }
+  async execute({
+    id,
+  }: FindScheduleInputDto): Promise<FindScheduleOutputDto | undefined> {
+    const response = await this._scheduleRepository.find(id);
+    if (response) {
+      return {
+        student: response.student,
+        curriculum: response.curriculum,
+        lessonsList: response.lessonsList,
+      };
+    } else {
+      return response;
+    }
+  }
+}
