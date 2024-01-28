77c02b271b47604eae941bcf8599bfbdb4b6b849
PauloCSantos <paulopcsj@hotmail.com>
2024-01-28 20:18:55 -0300

    feat: Implementado os usecases de evaluation

---
+++ backend/src/application/usecases/evaluation-note-attendance-management/evaluation/findAllEvaluation.usecase.ts
@@ -0,0 +1,32 @@
+import {
+  FindAllEvaluationInputDto,
+  FindAllEvaluationOutputDto,
+} from '@/application/dto/evaluation-note-attendance-management/evaluation-usecase.dto';
+import UseCaseInterface from '../../@shared/use-case.interface';
+import EvaluationGateway from '@/modules/evaluation-note-attendance-management/evaluation/gateway/evaluation.gateway';
+
+export default class FindAllEvaluation
+  implements
+    UseCaseInterface<FindAllEvaluationInputDto, FindAllEvaluationOutputDto>
+{
+  private _evaluationRepository: EvaluationGateway;
+
+  constructor(evaluationRepository: EvaluationGateway) {
+    this._evaluationRepository = evaluationRepository;
+  }
+  async execute({
+    offset,
+    quantity,
+  }: FindAllEvaluationInputDto): Promise<FindAllEvaluationOutputDto> {
+    const results = await this._evaluationRepository.findAll(offset, quantity);
+
+    const result = results.map(evaluation => ({
+      teacher: evaluation.teacher,
+      lesson: evaluation.lesson,
+      type: evaluation.type,
+      value: evaluation.value,
+    }));
+
+    return result;
+  }
+}
