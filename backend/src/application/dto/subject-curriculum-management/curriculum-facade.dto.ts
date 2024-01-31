c0d49b7a39708a9003f1291adea6a8444246e819
PauloCSantos <paulopcsj@hotmail.com>
2024-01-31 10:56:18 -0300

    feat: Implementado os Dtos para a facade de curriculum e subject

---
+++ backend/src/application/dto/subject-curriculum-management/curriculum-facade.dto.ts
@@ -0,0 +1,61 @@
+export interface FindCurriculumInputDto {
+  id: string;
+}
+export interface FindCurriculumOutputDto {
+  name: string;
+  yearsToComplete: number;
+  subjectsList: string[];
+}
+
+export interface FindAllCurriculumInputDto {
+  quantity?: number;
+  offset?: number;
+}
+export interface FindAllCurriculumOutputDto
+  extends Array<{
+    name: string;
+    yearsToComplete: number;
+    subjectsList: string[];
+  }> {}
+
+export interface CreateCurriculumInputDto {
+  name: string;
+  yearsToComplete: number;
+  subjectsList: string[];
+}
+export interface CreateCurriculumOutputDto {
+  id: string;
+}
+
+export interface UpdateCurriculumInputDto {
+  id: string;
+  name?: string;
+  yearsToComplete?: number;
+}
+export interface UpdateCurriculumOutputDto {
+  name: string;
+  yearsToComplete: number;
+}
+
+export interface DeleteCurriculumInputDto {
+  id: string;
+}
+export interface DeleteCurriculumOutputDto {
+  message: string;
+}
+
+export interface AddSubjectsInputDto {
+  id: string;
+  newSubjectsList: string[];
+}
+export interface AddSubjectsOutputDto {
+  message: string;
+}
+
+export interface RemoveSubjectsInputDto {
+  id: string;
+  subjectsListToRemove: string[];
+}
+export interface RemoveSubjectsOutputDto {
+  message: string;
+}
