2add6a5ff38455cce7c8ce7b941124b2a0ea81e8
PauloCSantos <paulopcsj@hotmail.com>
2024-02-01 08:44:05 -0300

    feat: Implementado o dto das facades do modulo evaluation-note-attendance-management

---
+++ backend/src/application/dto/evaluation-note-attendance-management/attendance-facade.dto.ts
@@ -0,0 +1,71 @@
+export interface FindAttendanceInputDto {
+  id: string;
+}
+export interface FindAttendanceOutputDto {
+  lesson: string;
+  date: Date;
+  hour: string;
+  day: string;
+  studentsPresent: string[];
+}
+
+export interface FindAllAttendanceInputDto {
+  quantity?: number;
+  offset?: number;
+}
+export interface FindAllAttendanceOutputDto
+  extends Array<{
+    lesson: string;
+    date: Date;
+    hour: string;
+    day: string;
+    studentsPresent: string[];
+  }> {}
+
+export interface CreateAttendanceInputDto {
+  lesson: string;
+  date: Date;
+  hour: Hour;
+  day: DayOfWeek;
+  studentsPresent: string[];
+}
+export interface CreateAttendanceOutputDto {
+  id: string;
+}
+
+export interface UpdateAttendanceInputDto {
+  id: string;
+  lesson?: string;
+  date?: Date;
+  hour?: Hour;
+  day?: DayOfWeek;
+}
+export interface UpdateAttendanceOutputDto {
+  lesson: string;
+  date: Date;
+  hour: string;
+  day: string;
+}
+
+export interface DeleteAttendanceInputDto {
+  id: string;
+}
+export interface DeleteAttendanceOutputDto {
+  message: string;
+}
+
+export interface AddStudentsInputDto {
+  id: string;
+  newStudentsList: string[];
+}
+export interface AddStudentsOutputDto {
+  message: string;
+}
+
+export interface RemoveStudentsInputDto {
+  id: string;
+  studentsListToRemove: string[];
+}
+export interface RemoveStudentsOutputDto {
+  message: string;
+}
---
+++ backend/src/application/dto/evaluation-note-attendance-management/evaluation-facade.dto.ts
@@ -0,0 +1,52 @@
+export interface FindEvaluationInputDto {
+  id: string;
+}
+export interface FindEvaluationOutputDto {
+  teacher: string;
+  lesson: string;
+  type: string;
+  value: number;
+}
+
+export interface FindAllEvaluationInputDto {
+  quantity?: number;
+  offset?: number;
+}
+export interface FindAllEvaluationOutputDto
+  extends Array<{
+    teacher: string;
+    lesson: string;
+    type: string;
+    value: number;
+  }> {}
+
+export interface CreateEvaluationInputDto {
+  teacher: string;
+  lesson: string;
+  type: string;
+  value: number;
+}
+export interface CreateEvaluationOutputDto {
+  id: string;
+}
+
+export interface UpdateEvaluationInputDto {
+  id: string;
+  teacher?: string;
+  lesson?: string;
+  type?: string;
+  value?: number;
+}
+export interface UpdateEvaluationOutputDto {
+  teacher: string;
+  lesson: string;
+  type: string;
+  value: number;
+}
+
+export interface DeleteEvaluationInputDto {
+  id: string;
+}
+export interface DeleteEvaluationOutputDto {
+  message: string;
+}
