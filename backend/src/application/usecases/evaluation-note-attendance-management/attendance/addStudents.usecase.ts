3f147abf6a30875f1a30a217daa21b19f1878595
PauloCSantos <paulopcsj@hotmail.com>
2024-02-01 08:49:07 -0300

    refactor: Agora addStudents e removeStudents usecase deserializa o retorno do repository para nao manipular diretamente a referencia

--- backend/src/application/usecases/evaluation-note-attendance-management/attendance/addStudents.usecase.ts
+++ backend/src/application/usecases/evaluation-note-attendance-management/attendance/addStudents.usecase.ts
@@ -4,6 +4,9 @@ import {
 } from '@/application/dto/evaluation-note-attendance-management/attendance-usecase.dto';
 import UseCaseInterface from '../../@shared/use-case.interface';
 import AttendanceGateway from '@/infraestructure/gateway/evaluation-note-attendance-management/attendance.gateway';
+import AttendanceMapper from '@/application/mapper/evaluation-note-attendance-management/attendance-usecase.mapper';
+import Attendance from '@/modules/evaluation-note-attendance-management/domain/entity/attendance.entity';
+import Id from '@/modules/@shared/domain/value-object/id.value-object';
 
 export default class AddStudents
   implements UseCaseInterface<AddStudentsInputDto, AddStudentsOutputDto>
@@ -19,10 +22,18 @@ export default class AddStudents
   }: AddStudentsInputDto): Promise<AddStudentsOutputDto> {
     const attendanceVerification = await this._attendanceRepository.find(id);
     if (!attendanceVerification) throw new Error('Attendance not found');
-
+    const attendanceObj = AttendanceMapper.toObj(attendanceVerification);
+    const newAttendance = JSON.parse(JSON.stringify(attendanceObj));
+    const attendance = new Attendance({
+      ...newAttendance,
+      id: new Id(newAttendance.id),
+      date: new Date(newAttendance.date),
+      hour: newAttendance.hour as Hour,
+      day: newAttendance.day as DayOfWeek,
+    });
     try {
       newStudentsList.forEach(studentId => {
-        attendanceVerification.addStudent(studentId);
+        attendance.addStudent(studentId);
       });
       const result = await this._attendanceRepository.addStudent(
         id,
--- backend/src/application/usecases/evaluation-note-attendance-management/attendance/removeStudents.usecase.ts
+++ backend/src/application/usecases/evaluation-note-attendance-management/attendance/removeStudents.usecase.ts
@@ -4,6 +4,9 @@ import {
 } from '@/application/dto/evaluation-note-attendance-management/attendance-usecase.dto';
 import UseCaseInterface from '../../@shared/use-case.interface';
 import AttendanceGateway from '@/infraestructure/gateway/evaluation-note-attendance-management/attendance.gateway';
+import AttendanceMapper from '@/application/mapper/evaluation-note-attendance-management/attendance-usecase.mapper';
+import Attendance from '@/modules/evaluation-note-attendance-management/domain/entity/attendance.entity';
+import Id from '@/modules/@shared/domain/value-object/id.value-object';
 
 export default class RemoveStudents
   implements UseCaseInterface<RemoveStudentsInputDto, RemoveStudentsOutputDto>
@@ -19,10 +22,18 @@ export default class RemoveStudents
   }: RemoveStudentsInputDto): Promise<RemoveStudentsOutputDto> {
     const attendanceVerification = await this._attendanceRepository.find(id);
     if (!attendanceVerification) throw new Error('Attendance not found');
-
+    const attendanceObj = AttendanceMapper.toObj(attendanceVerification);
+    const newAttendance = JSON.parse(JSON.stringify(attendanceObj));
+    const attendance = new Attendance({
+      ...newAttendance,
+      id: new Id(newAttendance.id),
+      date: new Date(newAttendance.date),
+      hour: newAttendance.hour as Hour,
+      day: newAttendance.day as DayOfWeek,
+    });
     try {
       studentsListToRemove.forEach(studentId => {
-        attendanceVerification.removeStudent(studentId);
+        attendance.removeStudent(studentId);
       });
       const result = await this._attendanceRepository.removeStudent(
         id,
