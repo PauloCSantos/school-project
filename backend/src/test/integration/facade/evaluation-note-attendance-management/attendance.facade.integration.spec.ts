03e7122c9aaad3a943b8bd2700d9e10a80220419
PauloCSantos <paulopcsj@hotmail.com>
2024-02-01 08:50:40 -0300

    test: Realizado os testes das facades do modulo evaluation-note-attendance-management

---
+++ backend/src/test/integration/facade/evaluation-note-attendance-management/attendance.facade.integration.spec.ts
@@ -0,0 +1,93 @@
+import AttendanceFacadeFactory from '@/application/factory/evaluation-note-attendance-management/attendance-facade.factory';
+import Id from '@/modules/@shared/domain/value-object/id.value-object';
+
+describe('Attendance facade integration test', () => {
+  const input = {
+    date: new Date(),
+    day: 'fri' as DayOfWeek,
+    hour: '06:50' as Hour,
+    lesson: new Id().id,
+    studentsPresent: [new Id().id, new Id().id, new Id().id],
+  };
+  const input2 = {
+    date: new Date(),
+    day: 'fri' as DayOfWeek,
+    hour: '06:50' as Hour,
+    lesson: new Id().id,
+    studentsPresent: [new Id().id, new Id().id, new Id().id],
+  };
+  const input3 = {
+    date: new Date(),
+    day: 'fri' as DayOfWeek,
+    hour: '06:50' as Hour,
+    lesson: new Id().id,
+    studentsPresent: [new Id().id, new Id().id, new Id().id],
+  };
+
+  it('should create an attendance using the facade', async () => {
+    const facade = AttendanceFacadeFactory.create();
+    const result = await facade.create(input);
+
+    expect(result.id).toBeDefined;
+  });
+  it('should find an attendance using the facade', async () => {
+    const facade = AttendanceFacadeFactory.create();
+    const result = await facade.create(input);
+    const userAttendance = await facade.find(result);
+
+    expect(userAttendance).toBeDefined;
+  });
+  it('should find all attendance using the facade', async () => {
+    const facade = AttendanceFacadeFactory.create();
+    await facade.create(input);
+    await facade.create(input2);
+    await facade.create(input3);
+    const allUsers = await facade.findAll({});
+
+    expect(allUsers.length).toBe(3);
+  });
+  it('should delete an attendance using the facade', async () => {
+    const facade = AttendanceFacadeFactory.create();
+    await facade.create(input);
+    const id2 = await facade.create(input2);
+    await facade.create(input3);
+    const result = await facade.delete({ id: id2.id });
+    const allUsers = await facade.findAll({});
+
+    expect(result.message).toBe('Operação concluída com sucesso');
+    expect(allUsers.length).toBe(2);
+  });
+  it('should update an attendance using the facade', async () => {
+    const facade = AttendanceFacadeFactory.create();
+    const id = await facade.create(input);
+
+    const result = await facade.update({
+      id: id.id,
+      lesson: new Id().id,
+    });
+
+    expect(result).toBeDefined;
+  });
+  it('should add students to the attendance using the facade', async () => {
+    const facade = AttendanceFacadeFactory.create();
+    const id = await facade.create(input);
+
+    const result = await facade.addStudents({
+      id: id.id,
+      newStudentsList: [new Id().id],
+    });
+
+    expect(result).toBeDefined;
+  });
+  it('should remove students to the attendance using the facade', async () => {
+    const facade = AttendanceFacadeFactory.create();
+    const id = await facade.create(input);
+
+    const result = await facade.removeStudents({
+      id: id.id,
+      studentsListToRemove: [input.studentsPresent[0]],
+    });
+
+    expect(result).toBeDefined;
+  });
+});
---
+++ backend/src/test/integration/facade/evaluation-note-attendance-management/note.facade.integration.spec.ts
@@ -0,0 +1,65 @@
+import NoteFacadeFactory from '@/application/factory/evaluation-note-attendance-management/note-facade.factory';
+import Id from '@/modules/@shared/domain/value-object/id.value-object';
+
+describe('Note facade integration test', () => {
+  const input = {
+    evaluation: new Id().id,
+    student: new Id().id,
+    note: 10,
+  };
+  const input2 = {
+    evaluation: new Id().id,
+    student: new Id().id,
+    note: 10,
+  };
+  const input3 = {
+    evaluation: new Id().id,
+    student: new Id().id,
+    note: 10,
+  };
+
+  it('should create an Note using the facade', async () => {
+    const facade = NoteFacadeFactory.create();
+    const result = await facade.create(input);
+
+    expect(result.id).toBeDefined;
+  });
+  it('should find an Note using the facade', async () => {
+    const facade = NoteFacadeFactory.create();
+    const result = await facade.create(input);
+    const Note = await facade.find(result);
+
+    expect(Note).toBeDefined;
+  });
+  it('should find all Note using the facade', async () => {
+    const facade = NoteFacadeFactory.create();
+    await facade.create(input);
+    await facade.create(input2);
+    await facade.create(input3);
+    const alls = await facade.findAll({});
+
+    expect(alls.length).toBe(3);
+  });
+  it('should delete an Note using the facade', async () => {
+    const facade = NoteFacadeFactory.create();
+    await facade.create(input);
+    const id2 = await facade.create(input2);
+    await facade.create(input3);
+    const result = await facade.delete({ id: id2.id });
+    const alls = await facade.findAll({});
+
+    expect(result.message).toBe('Operação concluída com sucesso');
+    expect(alls.length).toBe(2);
+  });
+  it('should update an  Note using the facade', async () => {
+    const facade = NoteFacadeFactory.create();
+    const id = await facade.create(input);
+
+    const result = await facade.update({
+      id: id.id,
+      note: 4,
+    });
+
+    expect(result).toBeDefined;
+  });
+});
