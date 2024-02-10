4205f3dce310c85f0538ff8974aba3fce8f25d26
PauloCSantos <paulopcsj@hotmail.com>
2024-02-10 08:49:32 -0300

    refactor: a rota de user worker agora nao dem dependencia direta com o express

--- backend/src/interface/route/user-management/user-worker.route.ts
+++ backend/src/interface/route/user-management/user-worker.route.ts
@@ -1,73 +1,82 @@
-import { Request, Response } from 'express';
-import ExpressHttpGateway from '@/infraestructure/http/express-http.gateway';
 import { UserWorkerController } from '@/interface/controller/user-management/user-worker.controller';
 import {
   CreateUserWorkerInputDto,
   UpdateUserWorkerInputDto,
 } from '@/application/dto/user-management/worker-usecase.dto';
+import { HttpInterface } from '@/infraestructure/http/http.interface';
 
 export class UserWorkerRoute {
   constructor(
     private readonly userWorkerController: UserWorkerController,
-    private readonly httpGateway: ExpressHttpGateway
+    private readonly httpGateway: HttpInterface
   ) {}
 
   public routes(): void {
-    this.httpGateway.get('/user-workers', this.findAllUserWorkers);
-    this.httpGateway.post('/user-workers', this.createUserWorker);
-    this.httpGateway.get('/user-workers/:id', this.findUserWorker);
-    this.httpGateway.put('/user-workers/:id', this.updateUserWorker);
-    this.httpGateway.delete('/user-workers/:id', this.deleteUserWorker);
+    this.httpGateway.get('/user-workers', (req: any, res: any) =>
+      this.findAllUserWorkers(req, res)
+    );
+    this.httpGateway.post('/user-workers', (req: any, res: any) =>
+      this.createUserWorker(req, res)
+    );
+    this.httpGateway.get('/user-workers/:id', (req: any, res: any) =>
+      this.findUserWorker(req, res)
+    );
+    this.httpGateway.put('/user-workers/:id', (req: any, res: any) =>
+      this.updateUserWorker(req, res)
+    );
+    this.httpGateway.delete('/user-workers/:id', (req: any, res: any) =>
+      this.deleteUserWorker(req, res)
+    );
   }
 
-  private async findAllUserWorkers(req: Request, res: Response): Promise<void> {
+  private async findAllUserWorkers(req: any, res: any): Promise<void> {
     try {
       const { quantity, offset } = req.body;
-      const response = await this.userWorkerController.findAll({
+      const any = await this.userWorkerController.findAll({
         quantity,
         offset,
       });
-      res.status(200).json(response);
+      res.status(200).json(any);
     } catch (error) {
       res.status(204).json({ error });
     }
   }
-  private async createUserWorker(req: Request, res: Response): Promise<void> {
+  private async createUserWorker(req: any, res: any): Promise<void> {
     try {
       const input = req.body as CreateUserWorkerInputDto;
-      const response = await this.userWorkerController.create(input);
-      res.status(201).json(response);
+      const any = await this.userWorkerController.create(input);
+      res.status(201).json(any);
     } catch (error) {
       res.status(400).json({ error });
     }
   }
-  private async findUserWorker(req: Request, res: Response): Promise<void> {
+  private async findUserWorker(req: any, res: any): Promise<void> {
     try {
       const { id } = req.params;
       const input = { id };
-      const response = await this.userWorkerController.find(input);
-      res.status(200).json(response);
+      const any = await this.userWorkerController.find(input);
+      res.status(200).json(any);
     } catch (error) {
       res.status(404).json({ error });
     }
   }
-  private async updateUserWorker(req: Request, res: Response): Promise<void> {
+  private async updateUserWorker(req: any, res: any): Promise<void> {
     try {
       const { id } = req.params;
       const input: UpdateUserWorkerInputDto = req.body;
       input.id = id;
-      const response = await this.userWorkerController.update(input);
-      res.status(200).json(response);
+      const any = await this.userWorkerController.update(input);
+      res.status(200).json(any);
     } catch (error) {
       res.status(404).json({ error });
     }
   }
-  private async deleteUserWorker(req: Request, res: Response): Promise<void> {
+  private async deleteUserWorker(req: any, res: any): Promise<void> {
     try {
       const { id } = req.params;
       const input = { id };
-      const response = await this.userWorkerController.delete(input);
-      res.status(204).json({ response });
+      const any = await this.userWorkerController.delete(input);
+      res.status(204).json({ any });
     } catch (error) {
       res.status(404).json({ error });
     }
