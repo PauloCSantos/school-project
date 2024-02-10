47313e72df22a846ce642c69fa3e8029a37b881b
PauloCSantos <paulopcsj@hotmail.com>
2024-02-10 08:48:44 -0300

    refactor: a rota de user student agora nao dem dependencia direta com o express

--- backend/src/interface/route/user-management/user-student.route.ts
+++ backend/src/interface/route/user-management/user-student.route.ts
@@ -1,77 +1,82 @@
-import { Request, Response } from 'express';
-
-import ExpressHttpGateway from '@/infraestructure/http/express-http.gateway';
 import { UserStudentController } from '@/interface/controller/user-management/user-student.controller';
 import {
   CreateUserStudentInputDto,
   UpdateUserStudentInputDto,
 } from '@/application/dto/user-management/student-usecase.dto';
+import { HttpInterface } from '@/infraestructure/http/http.interface';
 
 export class UserStudentRoute {
   constructor(
     private readonly userStudentController: UserStudentController,
-    private readonly httpGateway: ExpressHttpGateway
+    private readonly httpGateway: HttpInterface
   ) {}
 
   public routes(): void {
-    this.httpGateway.get('/user-students', this.findAllUserStudents);
-    this.httpGateway.post('/user-students', this.createUserStudent);
-    this.httpGateway.get('/user-students/:id', this.findUserStudent);
-    this.httpGateway.put('/user-students/:id', this.updateUserStudent);
-    this.httpGateway.delete('/user-students/:id', this.deleteUserStudent);
+    this.httpGateway.get('/user-students', (req: any, res: any) =>
+      this.findAllUserStudents(req, res)
+    );
+    this.httpGateway.post('/user-students', (req: any, res: any) =>
+      this.createUserStudent(req, res)
+    );
+    this.httpGateway.get('/user-students/:id', (req: any, res: any) =>
+      this.findUserStudent(req, res)
+    );
+    this.httpGateway.put('/user-students/:id', (req: any, res: any) =>
+      this.updateUserStudent(req, res)
+    );
+    this.httpGateway.delete('/user-students/:id', (req: any, res: any) =>
+      this.deleteUserStudent(req, res)
+    );
   }
 
-  private async findAllUserStudents(
-    req: Request,
-    res: Response
-  ): Promise<void> {
+  private async findAllUserStudents(req: any, res: any): Promise<void> {
     try {
       const { quantity, offset } = req.body;
-      const response = await this.userStudentController.findAll({
+      const any = await this.userStudentController.findAll({
         quantity,
         offset,
       });
-      res.status(200).json(response);
+      res.status(200).json(any);
     } catch (error) {
       res.status(204).json({ error });
     }
   }
-  private async createUserStudent(req: Request, res: Response): Promise<void> {
+  private async createUserStudent(req: any, res: any): Promise<void> {
     try {
       const input = req.body as CreateUserStudentInputDto;
-      const response = await this.userStudentController.create(input);
-      res.status(201).json(response);
+      const any = await this.userStudentController.create(input);
+      res.status(201).json(any);
     } catch (error) {
       res.status(400).json({ error });
     }
   }
-  private async findUserStudent(req: Request, res: Response): Promise<void> {
+  private async findUserStudent(req: any, res: any): Promise<void> {
     try {
       const { id } = req.params;
       const input = { id };
-      const response = await this.userStudentController.find(input);
-      res.status(200).json(response);
+      const any = await this.userStudentController.find(input);
+      res.status(200).json(any);
     } catch (error) {
       res.status(404).json({ error });
     }
   }
-  private async updateUserStudent(req: Request, res: Response): Promise<void> {
+  private async updateUserStudent(req: any, res: any): Promise<void> {
     try {
       const { id } = req.params;
       const input: UpdateUserStudentInputDto = req.body;
       input.id = id;
-      const response = await this.userStudentController.update(input);
-      res.status(200).json(response);
+      const any = await this.userStudentController.update(input);
+      res.status(200).json(any);
     } catch (error) {
       res.status(404).json({ error });
     }
   }
-  private async deleteUserStudent(req: Request, res: Response): Promise<void> {
+  private async deleteUserStudent(req: any, res: any): Promise<void> {
     try {
       const { id } = req.params;
       const input = { id };
-      const response = await this.userStudentController.delete(input);
-      res.status(204).json({ response });
+      const any = await this.userStudentController.delete(input);
+      res.status(204).json({ any });
     } catch (error) {
       res.status(404).json({ error });
     }
