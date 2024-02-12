71781e1ff902a47d2a88134ae9d880a36bdea427
PauloCSantos <paulopcsj@hotmail.com>
2024-02-12 10:17:27 -0300

    refactor: note agora nao esta acoplado com express

--- backend/src/interface/route/evaluation-note-attendance-management/note.route.ts
+++ backend/src/interface/route/evaluation-note-attendance-management/note.route.ts
@@ -1,26 +1,35 @@
-import { Request, Response } from 'express';
-import ExpressHttpGateway from '@/infraestructure/http/express-http.gateway';
 import { NoteController } from '@/interface/controller/evaluation-note-attendance-management/note.controller';
 import {
   CreateNoteInputDto,
   UpdateNoteInputDto,
 } from '@/application/dto/evaluation-note-attendance-management/note-usecase.dto';
+import { HttpInterface } from '@/infraestructure/http/http.interface';
 
 export class NoteRoute {
   constructor(
     private readonly noteController: NoteController,
-    private readonly httpGateway: ExpressHttpGateway
+    private readonly httpGateway: HttpInterface
   ) {}
 
   public routes(): void {
-    this.httpGateway.get('/note', this.findAllNotes);
-    this.httpGateway.post('/note', this.createNote);
-    this.httpGateway.get('/note/:id', this.findNote);
-    this.httpGateway.put('/note/:id', this.updateNote);
-    this.httpGateway.delete('/note/:id', this.deleteNote);
+    this.httpGateway.get('/note', (req: any, res: any) =>
+      this.findAllNotes(req, res)
+    );
+    this.httpGateway.post('/note', (req: any, res: any) =>
+      this.createNote(req, res)
+    );
+    this.httpGateway.get('/note/:id', (req: any, res: any) =>
+      this.findNote(req, res)
+    );
+    this.httpGateway.patch('/note/:id', (req: any, res: any) =>
+      this.updateNote(req, res)
+    );
+    this.httpGateway.delete('/note/:id', (req: any, res: any) =>
+      this.deleteNote(req, res)
+    );
   }
 
-  private async findAllNotes(req: Request, res: Response): Promise<void> {
+  private async findAllNotes(req: any, res: any): Promise<void> {
     try {
       const { quantity, offset } = req.body;
       const response = await this.noteController.findAll({
@@ -32,7 +41,7 @@ export class NoteRoute {
       res.status(204).json({ error });
     }
   }
-  private async createNote(req: Request, res: Response): Promise<void> {
+  private async createNote(req: any, res: any): Promise<void> {
     try {
       const input = req.body as CreateNoteInputDto;
       const response = await this.noteController.create(input);
@@ -41,7 +50,7 @@ export class NoteRoute {
       res.status(400).json({ error });
     }
   }
-  private async findNote(req: Request, res: Response): Promise<void> {
+  private async findNote(req: any, res: any): Promise<void> {
     try {
       const { id } = req.params;
       const input = { id };
@@ -51,7 +60,7 @@ export class NoteRoute {
       res.status(404).json({ error });
     }
   }
-  private async updateNote(req: Request, res: Response): Promise<void> {
+  private async updateNote(req: any, res: any): Promise<void> {
     try {
       const { id } = req.params;
       const input: UpdateNoteInputDto = req.body;
@@ -62,7 +71,7 @@ export class NoteRoute {
       res.status(404).json({ error });
     }
   }
-  private async deleteNote(req: Request, res: Response): Promise<void> {
+  private async deleteNote(req: any, res: any): Promise<void> {
     try {
       const { id } = req.params;
       const input = { id };
