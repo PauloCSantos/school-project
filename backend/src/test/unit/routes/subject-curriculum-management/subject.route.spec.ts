f7b0f1b54636ecdd7388afc823ba843d34c507a4
PauloCSantos <paulopcsj@hotmail.com>
2024-02-14 15:22:13 -0300

    test: Realizado os testes unitarios das rotas do modulo subject-curriculum-management

---
+++ backend/src/test/unit/routes/subject-curriculum-management/subject.route.spec.ts
@@ -0,0 +1,88 @@
+import Id from '@/modules/@shared/domain/value-object/id.value-object';
+import supertest from 'supertest';
+import ExpressHttp from '@/infraestructure/http/express-http';
+import { SubjectController } from '@/interface/controller/subject-curriculum-management/subject.controller';
+import { SubjectRoute } from '@/interface/route/subject-curriculum-management/subject.route';
+
+const mockSubjectController = jest.fn(() => {
+  return {
+    create: jest.fn().mockResolvedValue({ id: new Id().id }),
+    find: jest.fn().mockResolvedValue({
+      name: 'Math',
+      description: 'Described a subject',
+    }),
+    findAll: jest.fn().mockResolvedValue([
+      {
+        name: 'Math',
+        description: 'Described a subject',
+      },
+      {
+        name: 'Spanish',
+        description: 'Described a subject',
+      },
+    ]),
+    update: jest.fn().mockResolvedValue({
+      name: 'Math',
+      subjectsList: [new Id().id, new Id().id, new Id().id],
+      yearsToComplete: 5,
+    }),
+    delete: jest.fn().mockResolvedValue({
+      message: 'Operação concluída com sucesso',
+    }),
+  } as unknown as SubjectController;
+});
+
+describe('SubjectRoute unit test', () => {
+  const subjectController = mockSubjectController();
+  const expressHttp = new ExpressHttp();
+  const subjectRoute = new SubjectRoute(subjectController, expressHttp);
+  subjectRoute.routes();
+  const app = expressHttp.getExpressInstance();
+
+  describe('POST /subject', () => {
+    it('should create a subject', async () => {
+      const response = await supertest(app).post('/subject').send({
+        name: 'Math',
+        description: 'Described a subject',
+      });
+      expect(response.status).toBe(201);
+      expect(subjectController.create).toHaveBeenCalled();
+      expect(response.body.id).toBeDefined();
+    });
+  });
+  describe('GET /subject/:id', () => {
+    it('should find a subject by ID', async () => {
+      const response = await supertest(app).get('/subject/123');
+      expect(response.status).toBe(200);
+      expect(subjectController.find).toHaveBeenCalled();
+      expect(response.body).toBeDefined();
+    });
+  });
+  describe('GET /subject/', () => {
+    it('should find all subjects', async () => {
+      const response = await supertest(app).get('/subject');
+      expect(response.status).toBe(200);
+      expect(subjectController.findAll).toHaveBeenCalled();
+      expect(response.body).toBeDefined;
+      expect(response.body.length).toBe(2);
+    });
+  });
+  describe('PATCH /subject/:id', () => {
+    it('should update a subject by ID', async () => {
+      const response = await supertest(app).patch('/subject/123').send({
+        description: 'New description',
+      });
+      expect(response.status).toBe(200);
+      expect(subjectController.update).toHaveBeenCalled();
+      expect(response.body).toBeDefined();
+    });
+  });
+  describe('DELETE /subject/:id', () => {
+    it('should delete a subject by ID', async () => {
+      const response = await supertest(app).delete('/subject/123');
+      expect(response.status).toBe(204);
+      expect(subjectController.delete).toHaveBeenCalled();
+      expect(response.body.message).toBeDefined;
+    });
+  });
+});
