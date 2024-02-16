4dc88fd53ad74622e870a1bae9d885a7dd73ffde
PauloCSantos <paulopcsj@hotmail.com>
2024-02-16 14:33:06 -0300

    refactor: Todos updateusecases foram alterados para validar quando undefined e inserido um trycatch

--- backend/src/application/usecases/user-management/administrator/updateUserAdministrator.usecase.ts
+++ backend/src/application/usecases/user-management/administrator/updateUserAdministrator.usecase.ts
@@ -29,40 +29,50 @@ export default class UpdateUserAdministrator
     const userAdm = await this._userAdministratorRepository.find(id);
     if (!userAdm) throw new Error('User not found');
 
-    name?.firstName && (userAdm.name.firstName = name.firstName);
-    name?.middleName && (userAdm.name.middleName = name.middleName);
-    name?.lastName && (userAdm.name.lastName = name.lastName);
-    address?.street && (userAdm.address.street = address.street);
-    address?.city && (userAdm.address.city = address.city);
-    address?.zip && (userAdm.address.zip = address.zip);
-    address?.number && (userAdm.address.number = address.number);
-    address?.avenue && (userAdm.address.avenue = address.avenue);
-    address?.state && (userAdm.address.state = address.state);
-    email && (userAdm.email = email);
-    birthday && (userAdm.birthday = birthday);
-    graduation && (userAdm.graduation = graduation);
-    salary?.currency && (userAdm.salary.currency = salary.currency);
-    salary?.salary && (userAdm.salary.salary = salary.salary);
+    try {
+      name?.firstName !== undefined &&
+        (userAdm.name.firstName = name.firstName);
+      name?.middleName !== undefined &&
+        (userAdm.name.middleName = name.middleName);
+      name?.lastName !== undefined && (userAdm.name.lastName = name.lastName);
+      address?.street !== undefined &&
+        (userAdm.address.street = address.street);
+      address?.city !== undefined && (userAdm.address.city = address.city);
+      address?.zip !== undefined && (userAdm.address.zip = address.zip);
+      address?.number !== undefined &&
+        (userAdm.address.number = address.number);
+      address?.avenue !== undefined &&
+        (userAdm.address.avenue = address.avenue);
+      address?.state !== undefined && (userAdm.address.state = address.state);
+      email !== undefined && (userAdm.email = email);
+      birthday !== undefined && (userAdm.birthday = birthday);
+      graduation !== undefined && (userAdm.graduation = graduation);
+      salary?.currency !== undefined &&
+        (userAdm.salary.currency = salary.currency);
+      salary?.salary !== undefined && (userAdm.salary.salary = salary.salary);
 
-    const result = await this._userAdministratorRepository.update(userAdm);
+      const result = await this._userAdministratorRepository.update(userAdm);
 
-    return {
-      name: {
-        fullName: result.name.fullName(),
-        shortName: result.name.shortName(),
-      },
-      address: {
-        street: result.address.street,
-        city: result.address.city,
-        zip: result.address.zip,
-        number: result.address.number,
-        avenue: result.address.avenue,
-        state: result.address.state,
-      },
-      email: result.email,
-      birthday: result.birthday,
-      salary: result.salary.calculateTotalIncome(),
-      graduation: result.graduation,
-    };
+      return {
+        name: {
+          fullName: result.name.fullName(),
+          shortName: result.name.shortName(),
+        },
+        address: {
+          street: result.address.street,
+          city: result.address.city,
+          zip: result.address.zip,
+          number: result.address.number,
+          avenue: result.address.avenue,
+          state: result.address.state,
+        },
+        email: result.email,
+        birthday: result.birthday,
+        salary: result.salary.calculateTotalIncome(),
+        graduation: result.graduation,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 }
--- backend/src/application/usecases/user-management/worker/updateUserWorker.usecase.ts
+++ backend/src/application/usecases/user-management/worker/updateUserWorker.usecase.ts
@@ -25,38 +25,48 @@ export default class UpdateUserWorker
     const userAdm = await this._userWorkerRepository.find(id);
     if (!userAdm) throw new Error('User not found');
 
-    name?.firstName && (userAdm.name.firstName = name.firstName);
-    name?.middleName && (userAdm.name.middleName = name.middleName);
-    name?.lastName && (userAdm.name.lastName = name.lastName);
-    address?.street && (userAdm.address.street = address.street);
-    address?.city && (userAdm.address.city = address.city);
-    address?.zip && (userAdm.address.zip = address.zip);
-    address?.number && (userAdm.address.number = address.number);
-    address?.avenue && (userAdm.address.avenue = address.avenue);
-    address?.state && (userAdm.address.state = address.state);
-    email && (userAdm.email = email);
-    birthday && (userAdm.birthday = birthday);
-    salary?.currency && (userAdm.salary.currency = salary.currency);
-    salary?.salary && (userAdm.salary.salary = salary.salary);
+    try {
+      name?.firstName !== undefined &&
+        (userAdm.name.firstName = name.firstName);
+      name?.middleName !== undefined &&
+        (userAdm.name.middleName = name.middleName);
+      name?.lastName !== undefined && (userAdm.name.lastName = name.lastName);
+      address?.street !== undefined &&
+        (userAdm.address.street = address.street);
+      address?.city !== undefined && (userAdm.address.city = address.city);
+      address?.zip !== undefined && (userAdm.address.zip = address.zip);
+      address?.number !== undefined &&
+        (userAdm.address.number = address.number);
+      address?.avenue !== undefined &&
+        (userAdm.address.avenue = address.avenue);
+      address?.state !== undefined && (userAdm.address.state = address.state);
+      email !== undefined && (userAdm.email = email);
+      birthday !== undefined && (userAdm.birthday = birthday);
+      salary?.currency !== undefined &&
+        (userAdm.salary.currency = salary.currency);
+      salary?.salary !== undefined && (userAdm.salary.salary = salary.salary);
 
-    const result = await this._userWorkerRepository.update(userAdm);
+      const result = await this._userWorkerRepository.update(userAdm);
 
-    return {
-      name: {
-        fullName: result.name.fullName(),
-        shortName: result.name.shortName(),
-      },
-      address: {
-        street: result.address.street,
-        city: result.address.city,
-        zip: result.address.zip,
-        number: result.address.number,
-        avenue: result.address.avenue,
-        state: result.address.state,
-      },
-      email: result.email,
-      birthday: result.birthday,
-      salary: result.salary.calculateTotalIncome(),
-    };
+      return {
+        name: {
+          fullName: result.name.fullName(),
+          shortName: result.name.shortName(),
+        },
+        address: {
+          street: result.address.street,
+          city: result.address.city,
+          zip: result.address.zip,
+          number: result.address.number,
+          avenue: result.address.avenue,
+          state: result.address.state,
+        },
+        email: result.email,
+        birthday: result.birthday,
+        salary: result.salary.calculateTotalIncome(),
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 }
