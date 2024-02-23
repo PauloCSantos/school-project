10c012aa6dee3f227271dd174fe6a590d2a58570
PauloCSantos <paulopcsj@hotmail.com>
2024-02-23 08:46:05 -0300

    refactor: Agora as validacoes verificam o tipo do parametro

--- backend/src/util/validations.ts
+++ backend/src/util/validations.ts
@@ -1,17 +1,21 @@
 export function isNotEmpty(value: string): boolean {
+  if (typeof value !== 'string') return false;
   return value.length > 0;
 }
 
 export function isAlpha(value: string): boolean {
+  if (typeof value !== 'string') return false;
   const regexMatch = value.match(/^[a-zA-Z\s]+$/);
   return !!regexMatch;
 }
 
 export function maxLengthInclusive(value: string, maxlen: number): boolean {
+  if (typeof value !== 'string') return false;
   return value.length <= maxlen;
 }
 
 export function minLength(value: string, minlen: number): boolean {
+  if (typeof value !== 'string') return false;
   return value.length > minlen;
 }
 
@@ -20,10 +24,12 @@ export function isNumeric(value: number): boolean {
 }
 
 export function isGreaterZero(value: number): boolean {
+  if (typeof value !== 'number') return false;
   return value > 0;
 }
 
 export function validCurrency(value: 'R$' | '€' | '$'): boolean {
+  if (typeof value !== 'string') return false;
   const validCurrencies: Array<'R$' | '€' | '$'> = ['R$', '€', '$'];
   if (!validCurrencies.includes(value)) {
     return false;
@@ -33,6 +39,7 @@ export function validCurrency(value: 'R$' | '€' | '$'): boolean {
 }
 
 export function validEmail(value: string): boolean {
+  if (typeof value !== 'string') return false;
   const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(value);
 }
@@ -49,6 +56,7 @@ export function validDate(value: Date): boolean {
 }
 
 export function validCNPJ(value: string): boolean {
+  if (typeof value !== 'string') return false;
   value = value.replace(/\D/g, '');
   if (value.length !== 14) {
     return false;
@@ -60,6 +68,7 @@ export function validCNPJ(value: string): boolean {
 }
 
 export function validId(value: string): boolean {
+  if (typeof value !== 'string') return false;
   const regexMatch = value.match(
     /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
   );
@@ -72,15 +81,18 @@ export function areAllValuesUnique(arr: string[]): boolean {
 }
 
 export function validHour24h(value: string): boolean {
+  if (typeof value !== 'string') return false;
   const regex24h = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
   return regex24h.test(value);
 }
 
 export function validDay(value: string): boolean {
+  if (typeof value !== 'string') return false;
   return ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'].includes(value);
 }
 
 export function validNote(value: number): boolean {
+  if (typeof value !== 'number') return false;
   return value >= 0 && value <= 10;
 }
 
