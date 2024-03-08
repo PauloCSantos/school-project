f9c9455422e015c74f20afeaead8ab3754bbfb70
PauloCSantos <paulopcsj@hotmail.com>
2024-03-08 16:21:13 -0300

    feat: Implementado o facade dto para o AuthUser

---
+++ backend/src/application/dto/authentication-authorization-management/authUser-facade.dto.ts
@@ -0,0 +1,40 @@
+export interface FindAuthUserInputDto {
+  email: string;
+}
+export interface FindAuthUserOutputDto {
+  email: string;
+  masterId?: string;
+  role: RoleUsers;
+  isHashed: boolean;
+}
+
+export interface CreateAuthUserInputDto {
+  email: string;
+  password: string;
+  masterId?: string;
+  role: RoleUsers;
+  isHashed?: boolean;
+}
+export interface CreateAuthUserOutputDto {
+  email: string;
+}
+
+export interface UpdateAuthUserInputDto {
+  email: string;
+  authUserDataToUpdate: {
+    email?: string;
+    password?: string;
+    role?: RoleUsers;
+  };
+}
+export interface UpdateAuthUserOutputDto {
+  email: string;
+  role: RoleUsers;
+}
+
+export interface DeleteAuthUserInputDto {
+  email: string;
+}
+export interface DeleteAuthUserOutputDto {
+  message: string;
+}
