4b9ee3353e9fe7810838221754448380abb4b4b5
PauloCSantos <paulopcsj@hotmail.com>
2024-03-27 17:04:52 -0300

    feat: O setup de note agora implementa a nova configuracao da rota

--- backend/src/main/setups/evaluation-note-attendance-management/note.setup.ts
+++ backend/src/main/setups/evaluation-note-attendance-management/note.setup.ts
@@ -1,8 +1,10 @@
+import AuthUserMiddleware from '@/application/middleware/authUser.middleware';
 import CreateNote from '@/application/usecases/evaluation-note-attendance-management/note/createNote.usecase';
 import DeleteNote from '@/application/usecases/evaluation-note-attendance-management/note/deleteNote.usecase';
 import FindAllNote from '@/application/usecases/evaluation-note-attendance-management/note/findAllNote.usecase';
 import FindNote from '@/application/usecases/evaluation-note-attendance-management/note/findNote.usecase';
 import UpdateNote from '@/application/usecases/evaluation-note-attendance-management/note/updateNote.usecase';
+import tokenInstance from '@/infraestructure/config/tokenService/token-service.instance';
 import ExpressHttp from '@/infraestructure/http/express-http';
 import MemoryNoteRepository from '@/infraestructure/repositories/evaluation-note-attendance-management/memory-repository/note.repository';
 import { NoteController } from '@/interface/controller/evaluation-note-attendance-management/note.controller';
@@ -22,7 +24,15 @@ export default function initializeNote(express: ExpressHttp): void {
     updateNoteUsecase,
     deleteNoteUsecase
   );
-
-  const noteRoute = new NoteRoute(noteController, express);
+  const tokenService = tokenInstance();
+  const allowedRoles: RoleUsers[] = [
+    'master',
+    'administrator',
+    'student',
+    'teacher',
+    'worker',
+  ];
+  const authUserMiddleware = new AuthUserMiddleware(tokenService, allowedRoles);
+  const noteRoute = new NoteRoute(noteController, express, authUserMiddleware);
   noteRoute.routes();
 }
