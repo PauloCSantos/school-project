c81337665ffe95ca2f0edf9440e7bc024fde51b9
PauloCSantos <paulopcsj@hotmail.com>
2024-03-28 16:41:43 -0300

    feat: O setup de evaluation agora implementa a nova configuracao da rota

--- backend/src/main/setups/evaluation-note-attendance-management/evaluation.setup.ts
+++ backend/src/main/setups/evaluation-note-attendance-management/evaluation.setup.ts
@@ -1,8 +1,10 @@
+import AuthUserMiddleware from '@/application/middleware/authUser.middleware';
 import CreateEvaluation from '@/application/usecases/evaluation-note-attendance-management/evaluation/createEvaluation.usecase';
 import DeleteEvaluation from '@/application/usecases/evaluation-note-attendance-management/evaluation/deleteEvaluation.usecase';
 import FindAllEvaluation from '@/application/usecases/evaluation-note-attendance-management/evaluation/findAllEvaluation.usecase';
 import FindEvaluation from '@/application/usecases/evaluation-note-attendance-management/evaluation/findEvaluation.usecase';
 import UpdateEvaluation from '@/application/usecases/evaluation-note-attendance-management/evaluation/updateEvaluation.usecase';
+import tokenInstance from '@/infraestructure/config/tokenService/token-service.instance';
 import ExpressHttp from '@/infraestructure/http/express-http';
 import MemoryEvaluationRepository from '@/infraestructure/repositories/evaluation-note-attendance-management/memory-repository/evaluation.repository';
 import { EvaluationController } from '@/interface/controller/evaluation-note-attendance-management/evaluation.controller';
@@ -22,6 +24,18 @@ export default function initializeEvaluation(express: ExpressHttp): void {
     updateEvaluationUsecase,
     deleteEvaluationUsecase
   );
-  const evaluationRoute = new EvaluationRoute(evaluationController, express);
+  const tokenService = tokenInstance();
+  const allowedRoles: RoleUsers[] = [
+    'master',
+    'administrator',
+    'student',
+    'teacher',
+  ];
+  const authUserMiddleware = new AuthUserMiddleware(tokenService, allowedRoles);
+  const evaluationRoute = new EvaluationRoute(
+    evaluationController,
+    express,
+    authUserMiddleware
+  );
   evaluationRoute.routes();
 }
