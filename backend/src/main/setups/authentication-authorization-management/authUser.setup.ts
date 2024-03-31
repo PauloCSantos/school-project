6bae8500a46e8188c2515b661266da2b40d49bbf
PauloCSantos <paulopcsj@hotmail.com>
2024-03-31 08:58:56 -0300

    feat: O setup de authUser agora implementa a nova configuracao da rota

--- backend/src/main/setups/authentication-authorization-management/authUser.setup.ts
+++ backend/src/main/setups/authentication-authorization-management/authUser.setup.ts
@@ -1,8 +1,10 @@
+import AuthUserMiddleware from '@/application/middleware/authUser.middleware';
 import CreateAuthUser from '@/application/usecases/authentication-authorization-management/authUser/createAuthUser.usecase';
 import DeleteAuthUser from '@/application/usecases/authentication-authorization-management/authUser/deleteAuthUser.usecase';
 import FindAuthUser from '@/application/usecases/authentication-authorization-management/authUser/findAuthUser.usecase';
 import LoginAuthUser from '@/application/usecases/authentication-authorization-management/authUser/loginAuthUser.usecase';
 import UpdateAuthUser from '@/application/usecases/authentication-authorization-management/authUser/updateAuthUser.usecase';
+import tokenInstance from '@/infraestructure/config/tokenService/token-service.instance';
 import ExpressHttp from '@/infraestructure/http/express-http';
 import MemoryAuthUserRepository from '@/infraestructure/repositories/authentication-authorization-management/authUser.repository';
 import AuthUserController from '@/interface/controller/authentication-authorization-management/authUser.controller';
@@ -22,6 +24,19 @@ export default function initializeAuthUser(express: ExpressHttp): void {
     deleteAuthUser,
     loginAuthUser
   );
-  const authUserRoute = new AuthUserRoute(authUserController, express);
+  const tokenService = tokenInstance();
+  const allowedRoles: RoleUsers[] = [
+    'master',
+    'administrator',
+    'student',
+    'teacher',
+    'worker',
+  ];
+  const authUserMiddleware = new AuthUserMiddleware(tokenService, allowedRoles);
+  const authUserRoute = new AuthUserRoute(
+    authUserController,
+    express,
+    authUserMiddleware
+  );
   authUserRoute.routes();
 }
